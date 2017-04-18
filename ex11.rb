44ca342489725e41171a633b15876eae6a387167
Joey Zhou <jzhou@regis.edu>
2017-04-18 15:55:29 -0400

    Ask Questions

---
+++ ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight =  gets.chomp
+print "What does get do?"
+get = gets
+print "What does get chomps do?"
+getChomp = gets.chomp
+print "Whats the difference?\n"
+# gets alone will add \n behind the input
+print get
+# gets chomp will chomp off the \n
+print getChomp
+print "I dont know?\n"
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
