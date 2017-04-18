85ba0aa5f58edc3b2522ee8661843b76a71632f2
Joey Zhou <jzhou@regis.edu>
2017-04-18 11:56:50 -0400

    Printing, Printing

---
+++ ex08.rb
@@ -0,0 +1,17 @@
+formatter = "%{first} %{second} %{third} %{fourth}"
+# Testing on numbers and another name.
+test1 = "%{test1} %{test2} %{test3} %{test4}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
+
+puts test1 %{test1: "What ", test2: "is ", test3: "this", test4: "?"}
