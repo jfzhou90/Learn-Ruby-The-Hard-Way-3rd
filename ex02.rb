fca16270265f195be168485013adc2d137957126
Joey Zhou <jzhou@regis.edu>
2017-04-17 21:02:12 -0400

    Comments and Pound Characters

---
+++ ex02.rb
@@ -0,0 +1,15 @@
+# A comment, this is so that you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # And the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
+
+=begin
+This is another way to do multi line comment,
+however not recommended by Zed or the ruby community.
+it starts with =begin,and has to end with =end
+=end
