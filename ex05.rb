4e525c3f6ebdf21ba918b65812e5d0854b2c0d8a
Joey Zhou <jzhou@regis.edu>
2017-04-18 09:57:08 -0400

    More Variables and Printing

---
+++ ex05.rb
@@ -0,0 +1,17 @@
+name = 'Joey Zhou'
+age = 27 # true in 2017
+height = 65 # inches, I know, I'm short.
+weight = 140 # lbs, trying to gain a little bit more weight
+eyes = 'Black'
+teeth = 'White'
+hair = eyes # my hair is not my eyes, but they are the same color
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+#this line is tricky, try to get it exactly right.
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
