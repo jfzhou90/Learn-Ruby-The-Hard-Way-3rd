b8772ba91153f9173f909d4643416d888f623f2b
Joey Zhou <jzhou@regis.edu>
2017-04-21 16:06:24 -0400

    While Loops Study Drill

--- ex33a.rb
+++ ex33a.rb
@@ -1,10 +1,10 @@
-def test(x, y)
+def test(x, y, z)
   numbers = []
   while x < y
     puts "The top number is #{x}."
     numbers << x
     puts "Here are the numbers: ", numbers
-    x += 1
+    x += z
     puts "Now x is #{x}."
   end
   return numbers
@@ -14,7 +14,9 @@ puts "What's your smallest number? >"
 start = $stdin.gets.chomp.to_i
 puts "What's your biggest number? >"
 stop = $stdin.gets.chomp.to_i
-test(start, stop)
+puts "How much increment? >"
+increase = $stdin.gets.chomp.to_i
+test(start, stop, increase)
 
 puts "-" * 20
-puts "This only works comes with return function #{test(start, stop)}"
+puts "This only works comes with return function #{test(start, stop, increase)}"
