a459f22e21288879f84b14ee168fab93b3097cb2
Joey Zhou <jzhou@regis.edu>
2017-04-22 11:11:02 -0400

    Accessing elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,33 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1
+# Cardinal = as it is
+puts animals[1]
+
+# The third (3rd) animal
+# Ordinal = -1
+puts animals[2]
+
+# The first (1st) animal
+# Ordinal = -1
+puts animals[0]
+
+# The animal at 3
+# Cardinal = as it is
+puts animals[3]
+
+# The fifth (5th) animal
+# Ordinal
+puts animals[4]
+
+# The animal at 2
+# Cardinal
+puts animals[2]
+
+# The sixth (6th) animal
+# Ordinal
+puts animals[5]
+
+# The animal at 4
+# Cardinal
+puts animals[4]
